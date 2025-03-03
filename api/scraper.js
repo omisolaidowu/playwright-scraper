99f721047ead0b0520280c0ba2c892aa070985d7
omisolaidowu <omisolaidowu@gmail.com>
2025-03-03 14:17:17 +0100

    scraper exported as module

--- api/scraper.js
+++ api/scraper.js
@@ -3,19 +3,19 @@ const chromiumPath = require('@sparticuz/chromium').executablePath;
 
 module.exports = async (req, res) => {
     try {
-        const browser = await chromium.launch({
-            executablePath: await chromiumPath(),
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            headless: true,
-        });
+        // const browser = await chromium.launch({
+        //     executablePath: await chromiumPath(),
+        //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        //     headless: true,
+        // });
 
-        const page = await browser.newPage();
-        await page.goto('https://www.scrapingcourse.com/ecommerce/');
-        const title = await page.title();
+        // const page = await browser.newPage();
+        // await page.goto('https://www.scrapingcourse.com/ecommerce/');
+        // const title = await page.title();
 
-        await browser.close();
+        // await browser.close();
 
-        res.status(200).json({ title });
+        res.status(200).json({ title: 'hello world' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to scrape page' });
