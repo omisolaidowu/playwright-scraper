e3c48429dd705f0ef931a144d270174394d40b06
omisolaidowu <omisolaidowu@gmail.com>
2025-03-03 15:12:46 +0100

    scraper arranged

--- scraper.js
+++ scraper.js
@@ -3,18 +3,6 @@ const chromiumPath = require('@sparticuz/chromium').executablePath;
 
 module.exports = async (req, res) => {
     try {
-        // const browser = await chromium.launch({
-        //     executablePath: await chromiumPath(),
-        //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        //     headless: true,
-        // });
-
-        // const page = await browser.newPage();
-        // await page.goto('https://www.scrapingcourse.com/ecommerce/');
-        // const title = await page.title();
-
-        // await browser.close();
-
         res.status(200).json({ title: 'hello world' });
     } catch (error) {
         console.error(error);
