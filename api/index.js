a8f82627b2c43d2384255ac84eaf10c5fb56a18b
omisolaidowu <omisolaidowu@gmail.com>
2025-03-03 18:16:26 +0100

    test deploy

--- api/index.js
+++ api/index.js
@@ -1,28 +1,3 @@
-// const { chromium } = require('playwright-core');
-// const chromiumPath = require('@sparticuz/chromium').path;
-
-// module.exports = async (req, res) => {
-//     try {
-//         // Launch browser with correct Chromium binary
-//         const browser = await chromium.launch({
-//             executablePath: chromiumPath, // Use the lightweight Chromium binary
-//             args: ['--no-sandbox', '--disable-setuid-sandbox'],
-//             headless: true, // Keep it headless for minimal resource usage
-//         });
-
-//         const page = await browser.newPage();
-//         await page.goto('https://www.scrapingcourse.com/ecommerce/');
-//         const title = await page.title();
-
-//         await browser.close();
-
-//         res.status(200).json({ title });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Failed to scrape page' });
-//     }
-// };
-
 const { chromium } = require('playwright-core');
 const chromiumBinary = require('@sparticuz/chromium');
 
